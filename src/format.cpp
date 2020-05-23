cd2de0fe56c7ded41dfa98615f1f4c7f6ae0c477
James Ortiz <jamesaortiz75@gmail.com>
2020-05-22 20:34:00 -0400

    Formatted Clock functional

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,15 @@
 
 using std::string;
 
-// TODO: Complete this helper function
+// Done: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  string elapsed = "";
+  long hh, mm, ss;
+  hh = seconds / 3600;
+  mm = (seconds - hh*3600) / 60;
+  ss = seconds - hh*3600 - mm*60;
+  return std::to_string(hh) + ":" + std::to_string(mm) + ":" + std::to_string(ss);
+}
