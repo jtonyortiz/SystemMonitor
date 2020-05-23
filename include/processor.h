8c54b5b7c188a79a0a3bfa164b42522921975d02
James Ortiz <jamesaortiz75@gmail.com>
2020-05-22 20:27:27 -0400

    Processor header file completed

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,24 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
-class Processor {
+
+#include "linux_parser.h"
+
+using std::stof;
+using std::vector;
+using std::string;
+
+
+class Processor 
+{
  public:
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+ vector<float> prev_cpu_utils;
+ float IdleTime(vector<float>&);
+ float NonIdleTime(vector<float>&);
 };
 
 #endif
